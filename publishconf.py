77595d9a960838937153ffa8a1028c61ddd88e7e
Michael Sproul <micsproul@gmail.com>
2015-08-05 11:17:59 +1000

    Fix URLs by utilising SITEURL.

--- publishconf.py
+++ publishconf.py
@@ -10,13 +10,13 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://jeremycarlen.com'
+SITEURL = 'http://sproul.io/jeremy'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
